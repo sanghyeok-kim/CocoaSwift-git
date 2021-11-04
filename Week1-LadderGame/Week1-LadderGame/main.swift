7af9801c7a7686156bc99373bbe0b2e5c420ca51
sanghyeok-kim <slsnsi1212@gmail.com>
2021-11-04 16:48:30 +0900

    2. 사다리 인원 입력

--- Week1-LadderGame/Week1-LadderGame/main.swift
+++ Week1-LadderGame/Week1-LadderGame/main.swift
@@ -7,4 +7,30 @@
 
 import Foundation
 
-print("Hello, World!")
+
+//2. 사다리 인원 입력
+struct LadderGame {
+    static func inputParticipant() -> Int {
+        print("사다리 게임에 참여할 인원을 입력하세요 (2명~7명) : ", terminator: "")
+
+        let input: String = readLine() ?? ""
+        let inputNumbers: Int = Int(input) ?? -1
+
+        switch inputNumbers {
+        case ...1:
+            print("참여 인원은 2명 이상이어야 합니다.")
+        case 8...:
+            print("참여 인원은 7명 이하여야 합니다.")
+        default:
+            return inputNumbers
+        }
+        
+        return -1
+    }
+}
+
+LadderGame.inputParticipant()
+
+
+
+
