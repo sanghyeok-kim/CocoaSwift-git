461bd9488be431eb88edb32f6d942df1b4315fac
sanghyeok-kim <slsnsi1212@gmail.com>
2021-11-04 16:57:44 +0900

    3. 사다리 출력하기

--- Week1-LadderGame/Week1-LadderGame/main.swift
+++ Week1-LadderGame/Week1-LadderGame/main.swift
@@ -8,7 +8,8 @@
 import Foundation
 
 
-//2. 사다리 인원 입력
+//3. 사다리 출력하기
+
 struct LadderGame {
     static func inputParticipant() -> Int {
         print("사다리 게임에 참여할 인원을 입력하세요 (2명~7명) : ", terminator: "")
@@ -27,10 +28,39 @@ struct LadderGame {
         
         return -1
     }
+    
+    
+    static func printLadder(number: Int, height: Int) {
+        let rows = number - 1
+        let cols = height
+
+        var ladders = [[Int]]()
+        ladders = Array(repeating: Array(repeating: 0, count: rows), count: cols)
+        
+        
+        for i in 0..<cols {
+            for j in 0..<rows {
+                ladders[i][j] = Int.random(in: 0...1)
+            }
+        }
+
+        for i in 0..<cols {
+            for j in 0..<rows {
+                if j == 0 {
+                    print("|", terminator: "")
+                }
+                if ladders[i][j] == 0 {
+                    print("-|", terminator: "")
+                } else {
+                    print(" |", terminator: "")
+                }
+            }
+            print("")
+        }
+    }
 }
 
-LadderGame.inputParticipant()
-
-
-
-
+let participantNumber: Int = LadderGame.inputParticipant()
+if participantNumber != -1 {
+    LadderGame.printLadder(number: participantNumber, height: 4)
+}
