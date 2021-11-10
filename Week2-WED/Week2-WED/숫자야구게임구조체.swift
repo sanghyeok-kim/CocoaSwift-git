e70fbbcd0e5bf5baeedeccca27e71e4765dfe5d9
sanghyeok-kim <slsnsi1212@gmail.com>
2021-11-10 12:12:43 +0900

    3. 숫자야구게임구조체 - showGameScore() 제외한 나머지 완성

--- Week2-WED/Week2-WED/숫자야구게임구조체.swift
+++ Week2-WED/Week2-WED/숫자야구게임구조체.swift
@@ -12,52 +12,52 @@ struct NumberBaseball {
     
     
     mutating func newGame() {
-        secretNumber = Int.random(in: 100...111)
-        print(secretNumber)
+        secretNumber = Int.random(in: 100...999)
     }
     
     func hit(num: Int) -> (strikeCount: Int, ballCount: Int) {
         
         var countOfStrike = 0
         var countOfBall = 0
-        let result = (countOfStrike, countOfBall)
         
+        let secretFirstNumber = secretNumber % 10
+        let secretSecondNumber = secretNumber % 100 / 10
+        let secretThirdNumber = secretNumber / 100
         
-        let userFirstNumber = secretNumber % 10
-        let userSecondNumber = secretNumber % 100 / 10
-        let userThirdNumber = secretNumber / 100
+        let userFirstNumber = num % 10
+        let userSecondNumber = num % 100 / 10
+        let userThirdNumber = num / 100
         
-        let secretFirstNumber = num % 10
-        let secretSecondNumber = num % 100 / 10
-        let secretThirdNumber = num / 100
-        
-        let userNumberArr: [Int] = [userFirstNumber, userSecondNumber, userThirdNumber]
-        let secretNumberArr: [Int] = [secretFirstNumber, secretSecondNumber, secretThirdNumber]
-        
+        let userNumberArr: [Int] = [userThirdNumber, userSecondNumber, userFirstNumber]
+        let secretNumberArr: [Int] = [secretThirdNumber, secretSecondNumber, secretFirstNumber]
+ 
         let arrCount = userNumberArr.count
         
+        
+        //Strike 판별
         for i in 0..<arrCount {
-            userNumberArr.contains { (n) -> Bool in
-                print(n, secretNumberArr[i])
-                return n == secretNumberArr[i]
+            if userNumberArr[i] == secretNumberArr[i] {
+                countOfStrike += 1
             }
         }
         
+        //Ball 판별
+        for i in 0..<arrCount {
+            for j in 0..<arrCount {
+                if i == j {
+                    continue
+                }
+                if userNumberArr[i] == secretNumberArr[j] {
+                    countOfBall += 1
+                }
+            }
+        }
         
-        
-        
-        //var countOfStrike = 0
-        //var countOfBallcount = 0
-        
-        
-        
-        return (1, 1)
+        let result = (countOfStrike, countOfBall)
+        return result
     }
-    
-
 }
 
-//---main.swift---
 //var asd = NumberBaseball()
 //asd.newGame()
-//asd.hit(num: 123)
+//asd.hit(num: 124)
