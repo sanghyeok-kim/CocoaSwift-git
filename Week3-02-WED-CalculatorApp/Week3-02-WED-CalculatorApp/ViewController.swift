615dff5ce7e9c7b6c9e7ebeb0eca41c3d5174e75
sanghyeok-kim <slsnsi1212@gmail.com>
2021-11-17 23:32:05 +0900

    Create Week3-02-WED-CalculatorApp

--- Week3-02-WED-CalculatorApp/Week3-02-WED-CalculatorApp/ViewController.swift
+++ Week3-02-WED-CalculatorApp/Week3-02-WED-CalculatorApp/ViewController.swift
@@ -9,8 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var displayNumber = ""
+
+    
+    @IBOutlet weak var numberOutputLabel: UILabel!
+
+ 
+    @IBAction func tapNumberBtn(_ sender: UIButton) {
+        guard let number = sender.title(for: .normal) else { return }
+    
+        if self.displayNumber.count < 10 {
+            self.displayNumber += number
+            self.numberOutputLabel.text = self.displayNumber
+        }
+    
+    }
+    
+    @IBAction func divideBtn(_ sender: Any) {
+    }
+    
+    @IBAction func multiplyBtn(_ sender: Any) {
+    }
+    
+    @IBAction func subtractBtn(_ sender: Any) {
+    }
+    
+    @IBAction func eqaulBtn(_ sender: Any) {
+    }
+    
+    @IBAction func plusBtn(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
     }
 
