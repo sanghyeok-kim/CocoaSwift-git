aaa45f9f8f3c5e764b9ab1c1dce2f3ab3717f4b5
sanghyeok-kim <slsnsi1212@gmail.com>
2021-11-22 17:11:00 +0900

    Refactory - outlet collection 이용해서 text label 수정

--- Week4-01-MON-KoreanClockApp/Week4-01-MON-KoreanClockApp/ViewController.swift
+++ Week4-01-MON-KoreanClockApp/Week4-01-MON-KoreanClockApp/ViewController.swift
@@ -9,49 +9,13 @@ import UIKit
 import Foundation
 class ViewController: UIViewController {
     
-    
     @IBOutlet var labels: [UILabel]!
-    
-    //Outlet Collection에 대해 알았으니 다음부터는 이렇게 Label을 하나하나 하지말고 Collection을 쓰자 !!!
-    
-    //switch문은 모든 경우의 수를 다 case로 처리할 경우에만 사용하자 - 0의 경우를 처리하지 않아서, default가 실행되어 원치않은 결과가 실행됨
+    @IBOutlet var hours: [UILabel]!
+    @IBOutlet var minutes: [UILabel]!
+    @IBOutlet var tenMinutes: [UILabel]!
     
     @IBOutlet weak var jeon: UILabel!
     @IBOutlet weak var who: UILabel!
-    
-    @IBOutlet weak var young: UILabel!
-    @IBOutlet weak var yeol: UILabel!
-    
-    @IBOutlet weak var han: UILabel!
-    @IBOutlet weak var `do`: UILabel!
-    @IBOutlet weak var se: UILabel!
-    @IBOutlet weak var ne: UILabel!
-    @IBOutlet weak var da: UILabel!
-    @IBOutlet weak var yeo: UILabel!
-    @IBOutlet weak var sut: UILabel!
-    @IBOutlet weak var il: UILabel!
-    @IBOutlet weak var gop: UILabel!
-    @IBOutlet weak var yeo2: UILabel!
-    @IBOutlet weak var dulb: UILabel!
-    @IBOutlet weak var ah: UILabel!
-    @IBOutlet weak var hop: UILabel!
-    
-    @IBOutlet weak var ilbun: UILabel!
-    @IBOutlet weak var ebun: UILabel!
-    @IBOutlet weak var sambun: UILabel!
-    @IBOutlet weak var sabun: UILabel!
-    @IBOutlet weak var obun: UILabel!
-    @IBOutlet weak var yukbun: UILabel!
-    @IBOutlet weak var chillbun: UILabel!
-    @IBOutlet weak var palbun: UILabel!
-    @IBOutlet weak var gubun: UILabel!
-    
-    @IBOutlet weak var ship: UILabel!
-    @IBOutlet weak var eship: UILabel!
-    @IBOutlet weak var samship: UILabel!
-    @IBOutlet weak var saship: UILabel!
-    @IBOutlet weak var oship: UILabel!
-    
     @IBOutlet weak var sky: UILabel!
     @IBOutlet weak var secondLabel: UILabel!
     
@@ -93,45 +57,36 @@ class ViewController: UIViewController {
         //시
         let yeolHours = [10, 11, 12, 22, 23]
         if yeolHours.contains(hour) {
-            yeol.textColor = .white
-            
-            if hour % 10 == 1 {
-                han.textColor = .white
-            }
-            else if hour % 10 == 2 {
-                `do`.textColor = .white
-            }
-            if hour % 10 == 3 {
-                se.textColor = .white
-            }
+            hours[14].textColor = .white
         }
         
         let hourUnit = hour > 12 ? hour - 12 : hour
         switch hourUnit {
         case 0:
-            young.textColor = .white
+            hours[0].textColor = .white
         case 1:
-            han.textColor = .white
+            hours[1].textColor = .white
         case 2:
-            `do`.textColor = .white
+            hours[2].textColor = .white
         case 3:
-            se.textColor = .white
+            hours[3].textColor = .white
         case 4:
-            ne.textColor = .white
+            hours[4].textColor = .white
         case 5:
-            da.textColor = .white
+            hours[5].textColor = .white
+            hours[7].textColor = .white
         case 6:
-            yeo.textColor = .white
-            sut.textColor = .white
+            hours[6].textColor = .white
+            hours[7].textColor = .white
         case 7:
-            il.textColor = .white
-            gop.textColor = .white
+            hours[8].textColor = .white
+            hours[9].textColor = .white
         case 8:
-            yeo2.textColor = .white
-            dulb.textColor = .white
+            hours[10].textColor = .white
+            hours[11].textColor = .white
         case 9:
-            ah.textColor = .white
-            hop.textColor = .white
+            hours[12].textColor = .white
+            hours[13].textColor = .white
         default:
             return
         }
@@ -139,49 +94,49 @@ class ViewController: UIViewController {
  
         //분
         if minute >= 10 {
-            ship.textColor = .white
+            tenMinutes[0].textColor = .white
             
             if minute >= 50 {
-                oship.textColor = .white
+                tenMinutes[4].textColor = .white
             }
             else if minute >= 40 {
-                saship.textColor = .white
+                tenMinutes[3].textColor = .white
             }
             else if minute >= 30 {
-                samship.textColor = .white
+                tenMinutes[2].textColor = .white
             }
             else if minute >= 20 {
-                eship.textColor = .white
+                tenMinutes[1].textColor = .white
             }
         }
         
         let minuteUnit = minute % 10
         if minuteUnit == 1 {
-            ilbun.textColor = .white
+            minutes[0].textColor = .white
         }
         else if minuteUnit == 2 {
-            ebun.textColor = .white
+            minutes[1].textColor = .white
         }
         else if minuteUnit == 3 {
-            sambun.textColor = .white
+            minutes[2].textColor = .white
         }
         else if minuteUnit == 4 {
-            sabun.textColor = .white
+            minutes[3].textColor = .white
         }
         else if minuteUnit == 5 {
-            obun.textColor = .white
+            minutes[4].textColor = .white
         }
         else if minuteUnit == 6 {
-            yukbun.textColor = .white
+            minutes[5].textColor = .white
         }
         else if minuteUnit == 7 {
-            chillbun.textColor = .white
+            minutes[6].textColor = .white
         }
         else if minuteUnit == 8 {
-            palbun.textColor = .white
+            minutes[7].textColor = .white
         }
         else if minuteUnit == 9 {
-            gubun.textColor = .white
+            minutes[8].textColor = .white
         }
         
         
