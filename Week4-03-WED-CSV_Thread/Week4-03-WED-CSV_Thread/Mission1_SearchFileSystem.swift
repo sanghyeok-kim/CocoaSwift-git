e71245dc7d251d07c2568f24f15b3a2f427fbf1e
sanghyeok-kim <slsnsi1212@gmail.com>
2021-11-24 12:26:31 +0900

    Complete Mission1_SearchFileSystem

---
+++ Week4-03-WED-CSV_Thread/Week4-03-WED-CSV_Thread/Mission1_SearchFileSystem.swift
@@ -0,0 +1,84 @@
+//
+//  SearchFileSystem.swift
+//  Week4-03-WED-CSV_Thread
+//
+//  Created by 김상혁 on 2021/11/24.
+//
+
+import Foundation
+
+class FileSearch {
+    
+    //파일 찾기
+    func files(at path: String) -> Array<String> {
+        let fileManager = FileManager()
+        do {
+            let contents = try fileManager.contentsOfDirectory(atPath: path)
+            return contents
+            
+        } catch let error as NSError {
+            print("Error access directory: \(error)")
+            return []
+        }
+    }
+    
+    //파일 찾기(정렬 기능 추가)
+    func files(at path: String, sorted: Bool) -> Array<String> {
+        let fileManager = FileManager()
+        do {
+            let contents = try fileManager.contentsOfDirectory(atPath: path)
+            return sorted ? contents.sorted() : contents
+            
+        } catch let error as NSError {
+            print("Error access directory: \(error)")
+            return []
+        }
+    }
+    
+    
+    //파일 찾기(특정 확장자)
+    func files(at path: String, extensionType: String) -> Array<String> {
+        let fileManager = FileManager()
+        do {
+            let contents = try fileManager.contentsOfDirectory(atPath: path)
+            
+            /*
+            var result: [String] = []
+            for i in 0..<contents.count {
+                if contents[i].hasSuffix(extensionType) {
+                    result.append(contents[i])
+                }
+            }
+            return result
+            */
+            
+            
+            //위 코드를 filter를 사용해서 더 간단하게 줄일 수 있었다
+            let resultByFilter = contents.filter { $0.hasSuffix(extensionType) }
+            return resultByFilter
+            
+        } catch let error as NSError {
+            print("Error access directory: \(error)")
+            return []
+        }
+    }
+    
+    
+    //파일 존재 여부
+    func isExist(filename: String, at path: String) -> Bool {
+        let fileList = files(at: path)
+        return fileList.contains(filename)
+    }
+
+    //파일 존재 여부(여러 개의 파일)
+    func isExist(filename: String..., at path: String) -> Bool {
+        
+        let fileList = files(at: path)
+        for i in 0..<filename.count {
+            if fileList.contains(filename[i]) == false {
+                return false
+            }
+        }
+        return true
+    }
+}
--- Week4-03-WED-CSV_Thread/Week4-03-WED-CSV_Thread/main.swift
+++ Week4-03-WED-CSV_Thread/Week4-03-WED-CSV_Thread/main.swift
@@ -7,5 +7,29 @@
 
 import Foundation
 
-print("Hello, World!")
+
+/*--------------------연습문제1--------------------*/
+let targetPath = "/System/Library/Desktop Pictures"
+let contents = FileSearch()
+
+//파일 찾기
+//print(contents.files(at: targetPath))
+
+
+//파일 찾기(정렬 기능 추가)
+//print(contents.files(at: targetPath, sorted: true))
+
+
+//파일 찾기(특정 확장자)
+//print(contents.files(at: targetPath, extensionType: ".heic"))
+//print(contents.files(at: targetPath, extensionType: ".doc"))
+
+
+//파일 존재 여부
+//print(contents.isExist(filename: "hello Blue.heic", at: targetPath))
+
+
+//파일 존재 여부(여러 개의 파일)
+//print(contents.isExist(filename: "hello Blue.heic", "hello Red.heic", at: targetPath))
+
 
